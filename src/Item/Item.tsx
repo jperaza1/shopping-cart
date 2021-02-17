908fc33f7c8dcfcdacc9695085b5c6ca1f1b3870
jperaza1 <30508442+jperaza1@users.noreply.github.com>
2021-02-16 22:55:21 -0600

    subiendo los cambios

---
+++ src/Item/Item.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import  { Wrapper } from './Item.style';
+import { CartItemType } from '../App';
+import Button from '@material-ui/core/Button';
+
+type Props = {
+    item: CartItemType;
+    handleAddToCart: (clickedItem: CartItemType) => void;
+}
+
+const Item : React.FunctionComponent<Props> = ({ item,  handleAddToCart }) => (
+    <Wrapper>
+        <img src={item.image} alt={item.title} />
+        <div>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <h3>${item.price}</h3>
+        </div>
+        <Button onClick={() => handleAddToCart(item)}>Add to cart</Button>
+    </ Wrapper>
+);
+
+export default Item;
